330ab0f59eb63c43a5326d77c7cdd19939b51e6a
Abdullah Muhd <mariyahassangaya12@gmail.com>
2025-02-04 12:52:36 +0100

    FirstServerCommit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require ('express'); 
+const mongoose = require ('mongoose')
+
+const dburi = "mongodb+srv://AbdullahDB:db.key@cluster0.bmuva.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+mongoose.connect(dburi, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("MongoDB Connected Successfully"))
+  .catch(err => console.error("MongoDB Connection Failed:", err));
+
+const userSchema = new mongoose.Schema({
+        name: {
+            type: string,
+            required : true,
+        },
+        email: {
+            type : string, 
+            required : true,
+            unique : true
+        }, 
+        password: {
+            type : string, 
+            required : true, 
+        }, 
+        createdAt : {
+            type : Date, 
+            default: Date.now,
+        }, 
+        profilePhoto: {
+            type : string, 
+
+        },
+        bio : {
+            type : string, 
+        },
+
+
+});
+
+
+const User = mongoose.model('User', userSchema) 
+module.export = User;
+
+const app = express();
+const cors = require('cors');
+app.use(cors());
+app.get("/api", (req, res)=>{
+    res.json({"users":["Abdullah", "Muhammad", "Abdullah"]});
+}); 
+
+
+app.listen(5000, () =>{console.log('Eventta Server is running on port 5000')});
