08e384d1999bdb83a385d2b9baebbf41da839b00
Abdullah Muhd <mariyahassangaya12@gmail.com>
2025-02-07 14:30:45 +0100

    Initializing Users

--- server.js
+++ server.js
@@ -52,6 +52,38 @@ app.get("/api", (req, res) => {
     res.json({ "users": ["Abdullah", "Muhammad", "Abdullah"] });
 });
 
+
+app.use(express.json());
+//Posting User Registration Cridentials 
+app.post("/api/register", async (req, res)=>{
+    try {
+        const {name, email, password} = req.body;
+    
+//Validation
+    if(!name || !email || !password){
+        return res.status(400).json({message: "All fields are required"});
+    }
+//Check if user already exists
+const existingUser = await User.findOne ({email});
+if (existingUser){
+    return res.status(400).json({message: "User already exists"});
+}
+
+// Create new User 
+const newUser = new User({
+    name,
+    email,
+    password,
+});
+await newUser.save();
+res.status(201).json({message: "User registered successfully"});
+    }
+catch (error){
+    console.error(error);
+    res.status(500).json({message: "Server Error"});
+}
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
